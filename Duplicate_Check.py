9c315ea82a3c084f2a557e1db1778fd02d3a3139
facg88032 <facg88032@gmail.com>
2020-11-24 14:29:15 +0800

    update class_name

--- Duplicate_Check.py
+++ Duplicate_Check.py
@@ -2,16 +2,17 @@ import numpy as np
 import os
 
 
-def main():
-    list1=os.listdir('Process_data/Finish')
+def main(class_name):
+    list1=os.listdir('Process_data/Finish/'+class_name)
     for i in range(len(list1)):
-        a=np.load('Process_data/Finish/'+list1[i])
+        a=np.load('Process_data/Finish/'+class_name+'/'+list1[i])
         for j in range(i+1,len(list1)):
-            b=np.load('Process_data/Finish/'+list1[j])
+            b=np.load('Process_data/Finish/'+class_name+'/'+list1[j])
             #check data not duplicate
             if(a==b).all():
                 print(list1[i],list1[j]+' is same ')
 
 
 if __name__ == '__main__':
-    main()
+    class_name='dribble'
+    main(class_name)
--- Random_Select_image.py
+++ Random_Select_image.py
@@ -2,27 +2,27 @@ import numpy as np
 import os
 import random
 
-def SaveAppendList(Append_list,name,time):
+def SaveAppendList(Append_list,name,time,class_name):
     filename=name[:2]+'f'+name[2:4]+'_'+str(time)+'.txt'
-    with open('AppendList/Finish/'+filename, "w") as fs:
+    with open('AppendList/Finish/'+class_name+'/'+filename, "w") as fs:
         for i in Append_list:
             fs.write(str(i) + "\n")
     print('Successful Save '+filename)
 
-def Processdata(data,Append_list,name,time):
+def Processdata(data,Append_list,name,time,class_name):
     filename=name[:2] + 'f' + name[2:4]+'_'+str(time) + '.npy'
     process_data = []
     for i in Append_list:
         process_data.append(data[i])
     process_data = np.asarray(process_data)
-    np.save('Process_data/Finish/' + filename, process_data)
+    np.save('Process_data/Finish/'+class_name+'/' + filename, process_data)
     print('Successful process and Save to ' +filename)
 
 
-def random_selsct(times):
+def random_selsct(times,class_name):
 
     #load  All Files in the folder
-    for file in os.listdir('Process_data/Untreated'):
+    for file in os.listdir('Process_data/Untreated/'):
         data=np.load('Process_data/Untreated/'+file)
 
         #According to the specified number of times,
@@ -31,8 +31,8 @@ def random_selsct(times):
         for time in range(times):
             Append_list = random.sample(range(len(data)), 30)
             Append_list.sort()
-            SaveAppendList(Append_list,file,time+1)
-            Processdata(data,Append_list,file,time+1)
+            SaveAppendList(Append_list,file,time+1,class_name)
+            Processdata(data,Append_list,file,time+1,class_name)
 
             print('Random Select:', Append_list)
 
@@ -40,5 +40,7 @@ def random_selsct(times):
 
 
 if __name__ == '__main__':
+
+    class_name='dribble'
     times=30
-    random_selsct(times)
+    random_selsct(times,class_name)
