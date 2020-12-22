5d8ce281e5f0ce4ce13f973b8882e9adba8587c0
facg88032 <facg88032@gmail.com>
2020-12-22 17:57:35 +0800

    rewrite Duplicate_Check.py

--- Label_method/Duplicate_Check.py
+++ Label_method/Duplicate_Check.py
@@ -1,20 +1,6 @@
+import pandas as pd
 import numpy as np
-import os
 
-
-def main(path):
-    list1=os.listdir(path)
-    for i in range(len(list1)):
-        a=np.load(path+'/'+list1[i])
-        for j in range(i+1,len(list1)):
-            b=np.load(path+'/'+list1[j])
-            #check data not duplicate
-            if(a==b).all():
-                print(list1[i],list1[j]+' is same ')
-
-    print('Check Finish')
-
-if __name__ == '__main__':
-    class_name='dribble'
-    path='Process_data/half_'+class_name+'_e'
-    main(path)
+df=pd.DataFrame(np.load('dribble_data.npy').reshape(192,40*25*3))
+new_df =df.drop_duplicates()
+print(new_df)
