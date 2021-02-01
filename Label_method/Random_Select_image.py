6f9e2c7c88f3caa7b3350fae3b2a88fa524ccd64
facg88032 <facg88032@gmail.com>
2021-02-01 21:52:50 +0800

    upadte Random_Select_image.py

--- Label_method/Random_Select_image.py
+++ Label_method/Random_Select_image.py
@@ -4,20 +4,27 @@ from utils import Utils
 import configparser
 import argparse
 
-def main(class_name,num_select,times,folder_path):
-
+'''
+Perform N stratified random sampling of a label video clip  to enlarge the number of data
+ex: 40 frames randomly selected and reduced to 30 frames * N data
+'''
+def main(class_name,num_sample,times,folder_path):
+    #util 自行撰寫的小工具
     util=Utils()
     tmp = np.asarray([])
-    #load  All Files in the folder
     for file in os.listdir(folder_path+'/'+class_name):
+        #load data
         original_data=np.load(folder_path+'/'+class_name+'/'+file)
         for time in range(times):
-            SampleIndexs=util.StraifiedRandomSample(len(original_data),num_select)
-            process_data = util.DataProcess(original_data, SampleIndexs)
-            tmp=util.combine(tmp,process_data)
-
+            #Generate stratified random sample Indexs
+            SampleIndexs=util.StratifiedRandomSample(len(original_data),num_sample)
+            # Convert the original data into sample data by Indexs
+            sample_data = util.DataProcess(original_data, SampleIndexs)
+            #combine the all sample data
+            tmp=util.combine(tmp,sample_data)
         print('Combine data shape:', tmp.shape)
-    total_data=tmp.reshape(int(len(tmp)/num_select),num_select,25,3)
+    total_data=tmp.reshape(int(len(tmp)/num_sample),num_sample,25,3)
+    #save all sample data
     np.save(class_name+'.npy',total_data)
     print('total number of data',len(total_data))
 
@@ -25,17 +32,15 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--folder_path','-f', default="Label_datasets", help="label datasets in folder")
-    parser.add_argument('--num_select','-s',default=30 ,help='select number of image')
+    parser.add_argument('--num_sample','-s',default=30 ,help='num_sample number of image')
     args = parser.parse_known_args()
     folder=args[0].folder_path
-    select=args[0].num_select
+    sample=args[0].num_select
     cf = configparser.ConfigParser()
     cf.read('config.ini')
     secs = cf.sections()
-
-
     for sec in secs:
         class_name = cf.get(sec, 'class_name')
         times = cf.get(sec, 'times')
-        main( class_name, select ,int(times), folder)
+        main( class_name, sample ,int(times), folder)
 
